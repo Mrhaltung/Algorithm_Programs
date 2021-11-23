1c4ee1ab53618240d94620a3fa8e54ac2b6bae74
Mrhaltung <92170743+Mrhaltung@users.noreply.github.com>
2021-11-23 18:43:11 +0530

    Binary Tree for String

---
+++ BinaryTreeString/BinaryTreeString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    public static int binarySearch(String[] arr, String x)
+    {
+        int l = 0, r = arr.Length - 1;
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+
+            int res = x.CompareTo(arr[m]);
+
+            if (res == 0)
+                return m;
+
+            if (res > 0)
+                l = m + 1;
+
+            else
+                r = m - 1;
+        }
+        return -1;
+    }
+
+    public static void Main(String[] args)
+    {
+        String[] arr = { "I", "am", "Good", "Boy" };
+        String x = "Good";
+        int result = binarySearch(arr, x);
+
+        if (result == -1)
+            Console.WriteLine("Element not present");
+        else
+            Console.WriteLine("Element found at "
+                            + "index " + result);
+    }
+}
