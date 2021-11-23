6a9a30d3e0d7501dd47e1180e39e83895ef4387e
Mrhaltung <92170743+Mrhaltung@users.noreply.github.com>
2021-11-23 18:59:39 +0530

    Prime Number Program

---
+++ Prime/Prime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isPrime = true;
+            int i, j;
+
+            Console.WriteLine(" Prime Number Between 1 to 100");
+            Console.WriteLine("\n Prime Numbers are : ");
+         
+            for (i = 2; i <= 10; i++)
+            {
+                for (j = 2; j <= 10; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write("\n " + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
