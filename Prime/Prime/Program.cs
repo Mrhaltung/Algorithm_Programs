6e10246d3cab0e6c001be8e188b7f0f1cd836ff4
Mrhaltung <92170743+Mrhaltung@users.noreply.github.com>
2021-11-24 10:46:21 +0530

    Prime Program

--- Prime/Prime/Program.cs
+++ Prime/Prime/Program.cs
@@ -9,12 +9,12 @@ namespace Prime
             bool isPrime = true;
             int i, j;
 
-            Console.WriteLine(" Prime Number Between 1 to 100");
+            Console.WriteLine(" Prime Number Between 1 to 1000");
             Console.WriteLine("\n Prime Numbers are : ");
          
-            for (i = 2; i <= 10; i++)
+            for (i = 2; i <= 1000; i++)
             {
-                for (j = 2; j <= 10; j++)
+                for (j = 2; j <= 1000; j++)
                 {
                     if (i != j && i % j == 0)
                     {
