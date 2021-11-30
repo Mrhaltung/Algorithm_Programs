aeea504c890c12bd0f8a128a0f193178e9c2d10e
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-30 10:42:00 +0530

    Add project files.

---
+++ Prime_Anagram_Palidrome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prime_Anagram_Palidrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Number, Anagram, Palindrome Program");
+            Console.WriteLine("Number between 1 to 1000 is prime number, anagram and  palindrome ");
+            PAPN.IsPrime();
+        }
+    }
+}
